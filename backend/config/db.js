89bf74685097c244fd71e0f363c534b31ad40ccf
Munawar Jamil <munawar.minhas777@gmail.com>
2025-01-27 13:06:11 +0500

    database connected using mongoose

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,14 @@
- 
+import mongoose from "mongoose";
+const db_connection = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.DB_CONNECTION_STRING, {
+         
+        });
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
+    }
+    }
+
+    export default db_connection;
--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+import db_connection from "./config/db.js";
+dotenv.config();
 
- 
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+app.listen(process.env.PORT || 4000, async () => {
+await db_connection();
+  console.log(`Server is running on ${process.env.PORT}`);
+});
